25b487b8297e6e35c7959d47d2a323f7cd7ee026
Aravindhancareer <79400350+Aravindhancareer@users.noreply.github.com>
2023-04-23 17:13:11 +0530

    Update Employee.cs

--- WebApp/Models/Employee.cs
+++ WebApp/Models/Employee.cs
@@ -2,11 +2,11 @@
 {
     public class Employee
     {
-        public int Id? { get; set; }
-        public string Fullname? { get; set; }
-        public string Department? { get; set; }
-        public string Email? { get; set; }
-        public string Phone? { get; set; }
-        public string Address? { get; set; }
+        public int Id { get; set; } = Id.Empty;
+        public string Fullname { get; set; } = Fullname.Empty;
+        public string Department { get; set; } = Department.Empty;
+        public string Email { get; set; } = Email.Empty;
+        public string Phone { get; set; } = Phone.Empty;
+        public string Address { get; set; } = Address.Empty;
     }
 }
